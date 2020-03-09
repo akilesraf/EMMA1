28b9510c2cab867dce6267952f2c7a69684db206
akilesraf <61919976+akilesraf@users.noreply.github.com>
2020-03-09 10:57:41 -0300

    $ git init Initialized empty Git repository in ~/djangogirls/.git/ $ git config --global user.name "akilesraf" $ git config --global user.email rafael.n.24@hotmail.com

---
+++ erp/models.py
@@ -0,0 +1,69 @@
+
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Clientes(models.Model):
+    clientes_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.clientes_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?' 
+    list_filter = ['pub_date']  
+    search_fields = ['clientes_text'] 
+   
+
+
+class Informes(models.Model):
+    clientes = models.ForeignKey(Clientes, on_delete=models.CASCADE)
+    informes_text = models.CharField(max_length=200)
+    Monto_Pesos = models.IntegerField(default=0)
+    Monto_Dolares= models.IntegerField(default=0)
+    Deposito =models.BooleanField(null=True)
+    Guiro = models.BooleanField(null=True)
+    Banco = models.CharField(max_length = 80)
+    Persona_o_entidad= models.CharField(max_length = 80)
+    Recivido =  models.BooleanField(null=True)
+    Enviado = models.BooleanField(null=True)
+    Pagos_web =  models.BooleanField(null=True)
+    simulador_Credito = models.IntegerField(default=0)
+    Monto_Total_credito = models.IntegerField(default=0)
+    Cuotas_Credito = models.IntegerField(default=0)
+    Sueldo = models.IntegerField(default=0)
+
+   
+    def __str__(self):
+        return self.informes_text
+
+
+class Pagos(models.Model):
+    clientes_text = models.CharField(max_length=200)
+    pagos_text = models.CharField(max_length=200)
+    cantidad = models.IntegerField(default=0)
+    efectivo = models.IntegerField(default=0)
+    tarjeta = models.IntegerField(default=0)
+    bancario = models.IntegerField(default=0)
+    adelanto = models.IntegerField(default=0)
+    sesenta =  models.IntegerField(default=0)
+    treinta =  models.IntegerField(default=0)
+    diez =  models.IntegerField(default=0)
+    def __str__(self):
+        return self.pagos_text       
+
+
+        
+
+
+
+
+
